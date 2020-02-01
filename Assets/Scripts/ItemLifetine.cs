9e954ae0109a67fea39b3240aac06c9644d7b09f
Jesus Urdaneta <jesusrafael.urdaneta@hotmail.com>
2020-02-01 02:16:08 -0400

    Agregando Timer para items e Item en prefabs

---
+++ Assets/Scripts/ItemLifetine.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemLifetine : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float lifetime = 4.0f;
+    void Start()
+    {
+         Destroy(gameObject, lifetime);
+    }
+
+    // Update is called once per frame
+    void Awake()
+    {
+        
+    }
+}
