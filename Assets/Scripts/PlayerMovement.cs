72da5dda8b9a9eaa1977e9163ed8d44e3b9a85dd
Jesus Urdaneta <jesusrafael.urdaneta@hotmail.com>
2020-02-01 00:19:32 -0400

    Agregando Player Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Vector3 pos;
+    bool complete;
+    bool input;
+
+    public int speed;
+
+    Ray ray;
+    void Start()
+    {
+    }
+    // Update is called once per framel
+    private void LateUpdate() {
+        input = Input.GetButton("Fire1");
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    }
+    void FixedUpdate()
+    {
+
+        float step = speed * Time.deltaTime;
+        if (input)
+        {
+
+            complete = true;
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                pos = hit.point;
+            }
+        }
+
+        if (complete){
+            transform.position = Vector3.MoveTowards(transform.position, pos, step);
+        }
+
+        if(transform.position == pos && complete){
+            complete = false;
+        }
+
+    }
+}
