5cbdbcdc83da5649fd8886e1f3ebbaeb5f1c2f60
eniliosarcos <enilio.sarcos@3davenue.net>
2020-02-01 11:42:07 -0400

    movement to enemies with navmesh

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour
 {
+    public Transform tower;
+    NavMeshAgent _agent;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _agent.SetDestination(new Vector3(tower.position.x,0,tower.position.z));
     }
 }
